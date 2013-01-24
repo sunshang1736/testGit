4b32a5e2e4e55f6a1f1f73a8d2f327f96c24dc31
sunshang1736 <sunshang1736@163.com>
2013-01-24 19:07:05 +0800

    hehe

---
+++ gitTest/gitTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by sunshang on 13-1-24.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
